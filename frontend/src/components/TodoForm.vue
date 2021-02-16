db3d7cfd05c2f37ea99fdb3d9c0f149e8aeb900e
azukiazusa1 <cycleippei@gmail.com>
2021-02-16 22:12:22 +0900

    todo form validation

--- frontend/src/components/TodoForm.vue
+++ frontend/src/components/TodoForm.vue
@@ -1,13 +1,20 @@
 <template>
-  <form novalidate @submit.prevent="handleSubmit">
+  <form
+    novalidate 
+    @submit.prevent="handleSubmit">
     <div class="form-group">
       <label for="title">Todoを追加する</label>
       <input 
         v-model="title"
         id="title" 
-        class="form-control" 
+        class="form-control"
+        :class="{ 'is-invalid': error }" 
         type="text" 
-        placeholder="タイトルを入力" />
+        placeholder="タイトルを入力"
+        required />
+      <div class="invalid-feedback">
+        タイトルを入力してください。
+      </div>
     </div>
   </form>
 </template>
@@ -18,15 +25,22 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   setup(_, { emit }) {
     const title = ref('')
+    const error = ref(false)
 
     const handleSubmit = () => {
+      if (title.value.trim() === '') {
+        error.value = true
+        return
+      }
+      error.value = false
       emit('on-submit', title.value)
       title.value = ''
     }
 
     return {
       title,
-      handleSubmit  
+      error,
+      handleSubmit
     }
   }
 })
