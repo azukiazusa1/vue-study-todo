374edf9a33f35677ad16060b3360e73653301014
azukiazusa1 <cycleippei@gmail.com>
2021-02-16 17:55:29 +0900

    the header

---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <nav class="navbar navbar-default bg-primary">
+      <div class="container">
+        <div class="navbar-header">
+          <span class="navbar-brand text-white">Vue Study Todo</span>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
