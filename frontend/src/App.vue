09f121e587684aa124f6d13b61f2b88d1ff10c6c
azukiazusa1 <cycleippei@gmail.com>
2021-02-16 18:21:45 +0900

    todolist

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <the-header />
+  <main>
+    <div class="container mt-4">
+      <todo-list />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TheHeader from './components/TheHeader.vue'
+import TodoList from './components/TodoList.vue'
 
 export default defineComponent({
   name: "App",
   components: {
-    TheHeader
+    TheHeader,
+    TodoList
   }
 });
 </script>
---
+++ frontend/src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>{{ todos }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Todo } from '../types'
+
+const todos: Todo[] = [
+  {
+    id: 1,
+    title: 'todo1',
+    done: true,
+  },
+  {
+    id: 2,
+    title: 'todo2',
+    done: false,
+  },
+  {
+    id: 3,
+    title: 'todo3',
+    done: true,
+  },
+]
+
+export default defineComponent({
+  setup() {
+    return {
+      todos
+    }
+  }
+})
+</script>
